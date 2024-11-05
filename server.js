a6df3a58ad2a9633d6ae832ee89047f4d781e7d9
Emmanuel1255 <emmmanuelkamanda1255@gmail.com>
2024-11-05 21:01:05 +0000

    fix

--- server.js
+++ server.js
@@ -247,7 +247,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
   console.log(`API URL: http://localhost:${PORT}/api`);
-  console.log(`Client URL: ${process.env.CLIENT_URL || 'http://localhost:5173'}`);
+  console.log(`Client URL: ${process.env.CLIENT_URL || 'https://tok-blog-cms.web.app/'}`);
   console.log(`Public path: ${path.join(__dirname, 'public')}`);
 });
 
